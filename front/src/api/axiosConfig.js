801b124614d758a63bcc545de52b235f5a3906d9
Cristia Acalo <cristianchoacalo@gmail.com>
2025-03-05 11:21:45 -0500

    changing string conexion v3

--- front/src/api/axiosConfig.js
+++ front/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { config } from "../../config/config";
 
 // const API_BASE_URL = "http://localhost:3000/api/v1";
-const API_BASE_URL = config.domain;
+const API_BASE_URL = config.domain + "/api/v1";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
